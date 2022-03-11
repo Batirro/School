688e216be6e6f6b9f56dd8633da59d5a1d364172
Batirro <batirro34@protonmail.com>
2022-03-11 22:13:28 +0100

    Update Zliczanie_znakow.cpp

--- Zliczanie_znakow.cpp
+++ Zliczanie_znakow.cpp
@@ -2,7 +2,72 @@
 
 using namespace std;
 
+void zlicz(int Licznik[], string tekst);
+void max_znak(int Licznik[]);
+void wyswietl(int Licznik[]);
+
 int main()
 {
-    cout << "Hello World" << endl;
+    int Licznik[26] = {0};
+    string tekst = " ";
+    ifstream inputfile("C:\\Users\\bartek\\Desktop\\jawny.txt");
+    while (!inputfile.eof())
+    {
+        inputfile >> tekst;
+    }
+    zlicz(Licznik, tekst);
+    max_znak(Licznik);
+    wyswietl(Licznik);
+    cout << "Plik zostal zapisany." << endl;
+    inputfile.close();
+    return 0;
+}
+
+void zlicz(int Licznik[], string tekst)
+{
+    char znak;
+    for (int i = 0; i < tekst.size(); i++)
+    {
+        znak = toupper(tekst[i]);
+        if (znak >= 'A' && znak <= 'Z')
+        {
+            Licznik[znak - 'A']++;
+        }
+    }
+}
+
+void max_znak(int Licznik[])
+{
+    int max_z = 0, temp;
+    ofstream outputfile("C:\\Users\\bartek\\Desktop\\wynik.txt");
+    for (int i = 0; i < 26; i++)
+    {
+        if (Licznik[i] > Licznik[max_z])
+        {
+            max_z = i;
+        }
+    }
+
+    temp = Licznik[max_z];
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (Licznik[i] == temp)
+        {
+            outputfile << "Najwiecej razy wystapila litera " << char(i + 'A') << " stalo sie to az " << temp << " razy.";
+        }
+    }
+}
+
+void wyswietl(int Licznik[])
+{
+    ofstream outputfile("C:\\Users\\bartek\\Desktop\\wynik.txt");
+    for (int i = 0; i < 26; i++)
+    {
+        if (Licznik[i] > 0)
+        {
+            outputfile << char(i + 'A') << " - " << Licznik[i] << endl;
+        }
+    }
+    outputfile.close();
 }
