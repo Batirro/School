6b5c702bf7d8a7d1da7d35d20fa59b29822459d3
Batirro <batirro34@protonmail.com>
2022-03-16 17:19:31 +0100

    Update Dlugosc_wyrazow.cpp

--- Dlugosc_wyrazow.cpp
+++ Dlugosc_wyrazow.cpp
@@ -7,35 +7,40 @@ void Liczenie_dl_wyrazow(string s);
 int main()
 {
 	string s = " ";
-	cout << "Podaj lancuch znakow: ";
-	getline(cin,s);
-	Liczenie_dl_wyrazow(s);
+	ifstream infile("C:\\Users\\bartek\\Desktop\\ciag.txt");
+	while (!infile.eof())
+	{
+		infile >> s;
+		Liczenie_dl_wyrazow(s);
+	}
+	cout << "Plik został zapisany!" << endl;
 	return 0;
 }
 
 void Liczenie_dl_wyrazow(string s)
 {
+	ofstream outfile("C:\\Users\\bartek\\Desktop\\ciagw.txt");
 	int licznik[11] = {}, poz1 = 0, poz2 = 0;
 	string temp = " ";
 	while (s.size() > 0)
+	{
+		poz1 = s.find(" ", poz1);
+		temp = s.substr(poz2, poz1);
+		if (temp.size() > 10)
 		{
-			poz1 = s.find(" ", poz1);
-			temp = s.substr(poz2,poz1);
-			if (temp.size() > 10)
-				{
-					cout << "Wyraz nie spelnia wymagan" << endl;
-					exit (1);
-				}
-			licznik[temp.size()]++;
-			s=s.erase(poz2,temp.size()+1);
-			poz1 = 0;
-			poz2 = 0;
+			cout << "Wyraz nie spelnia wymagan" << endl;
+			exit(1);
 		}
+		licznik[temp.size()]++;
+		s = s.erase(poz2, temp.size() + 1);
+		poz1 = 0;
+		poz2 = 0;
+	}
 	for (int i = 1; i < 10; i++)
+	{
+		if (licznik[i] > 0)
 		{
-			if(licznik[i] > 0)
-			{
-					cout << "Wyrazow " << i << " literowych jest " << licznik[i] << endl;
-			}
+			outfile << "Wyrazow " << i << " literowych jest " << licznik[i] << endl;
 		}
+	}
 }
