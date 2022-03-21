9b98ec60bfe60b63253644112276af273fc7200c
Batirro <93288860+Batirro@users.noreply.github.com>
2022-03-21 08:58:00 +0100

    Update Zliczanie_znaków_pl.cpp

--- Zliczanie_znaków_pl.cpp
+++ Zliczanie_znaków_pl.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -6,72 +7,88 @@ void zlicz(int Licznik[], string tekst);
 int max_znak(int Licznik[]);
 void wyswietl(int Licznik[]);
 
-const string alfabet_pl_m = "a¹bcædeêfghijkl³mnñoóprsœtuwyzŸ¿";
-const string alfabet_pl_d = "A¥BCÆDEÊFGHIJKL£MNÑOÓPRSŒTUWYZ¯";
+const string alfabet_pl_m = "aÄ…bcÄ‡deÄ™fghijklÅ‚mnÅ„oÃ³prsÅ›tuvwxyzÅºÅ¼";
+const string alfabet_pl_d = "AÄ„BCÄ†DEÄ˜FGHIJKLÅMNÅƒOÃ“PRSÅšTUVWXYZÅ¹Å»";
 
 
 int main()
 {
-	setConsoleCp(812);
+	SetConsoleCP(852);
 	setlocale(LC_ALL,"");
-    int Licznik[35] = {0};
-    string tekst = " ";
-    ifstream inputfile ("C:\\Users\\uczen_09\\Desktop\\ciag.txt");
-    while (!inputfile.eof())
-    {
-        inputfile >> tekst;
-        cout << tekst << " ";
-       // zlicz(Licznik, tekst);
-    }
-   // wyswietl(Licznik);
-   // cout << "Plik zostal zapisany" << endl;
-    inputfile.close();
-    return 0;
+	int Licznik[35] = {0};
+	string tekst = " ";
+	int i, j;
+	for(i = 0; i < 35; i++)
+		{
+			Licznik[i] = 0;
+		}
+	fstream inputfile ("C:\\Users\\uczen_09\\Desktop\\tajne.txt");
+	while(!inputfile.eof())
+		{
+			getline(inputfile, tekst);
+			for(i = 0; i < tekst.size(); i++)
+				{
+					j = alfabet_pl_m.find(tekst[i]);
+					if (j >= 0 && j < 35)
+						{
+							Licznik[j]++;
+						}
+					j = alfabet_pl_d.find(tekst[i]);
+					if (j >= 0 && j < 35)
+						{
+							Licznik[j]++;
+						}
+				}
+		}
+	wyswietl(Licznik);
+	cout << "Plik zostaÅ‚ pomyÅ›lnie zapisany!" << endl;
+	inputfile.close();
+	return 0;
 }
 
 void zlicz(int Licznik[], string tekst)
 {
-    char znak;
-    for (int i = 0; i < tekst.size(); i++)
-    {
-        znak = toupper(tekst[i]);
-        if (znak >= 'A' && znak <= '¯')
-        {
-            Licznik[znak - 'A']++;
-        }
-    }
+	char znak;
+	for (int i = 0; i < tekst.size(); i++)
+		{
+			znak = toupper(tekst[i]);
+			if (znak >= 'A' && znak <= '?')
+				{
+					Licznik[znak - 'A']++;
+				}
+		}
 }
 
-//int max_znak(int Licznik[])
+int max_znak(int Licznik[])
 {
-    int max_z = 0, temp;
-    for (int i = 0; i < 35; i++)
-    {
-        if (Licznik[i] > Licznik[max_z])
-        {
-            max_z = i;
-        }
-    }
+	int max_z = 0, temp;
+	for (int i = 0; i < 35; i++)
+		{
+			if (Licznik[i] > Licznik[max_z])
+				{
+					max_z = i;
+				}
+		}
 
-    temp = Licznik[max_z];
-    return temp;
+	temp = Licznik[max_z];
+	return temp;
 }
 
 void wyswietl(int Licznik[])
 {
-    ofstream outputfile("C:\\Users\\uczen_09\\Desktop\\ciagw.txt");
-    int pom = max_znak(Licznik), i1;
-    for (int i = 0; i < 35; i++)
-    {
-        if (Licznik[i] >= 0)
-        {
-            outputfile << char(i + 'A') << " - " << Licznik[i] << endl;
-        }
-     //   if (Licznik[i] == pom)
-        {
-            i1 = i;
-        }
-    }
-   // outputfile << "Najwiecej razy wystapila litera " << char(i1 + 'A') << " stalo sie to az " << pom << " razy" << endl;
-    outputfile.close();
+	ofstream outputfile("C:\\Users\\uczen_09\\Desktop\\czestosc.txt");
+	int pom = max_znak(Licznik), i1;
+	for (int i = 0; i < 35; i++)
+		{
+			if (Licznik[i] >= 0)
+				{
+					outputfile << alfabet_pl_d[i] << " - " << Licznik[i] << endl;
+				}
+			if (Licznik[i] == pom)
+			{
+				i1 = i;
+			}
+		}
+	outputfile << "Najwiecej razy wystapila litera " << alfabet_pl_d[i1] << " stalo sie to az " << pom << " razy" << endl;
+	outputfile.close();
 }
