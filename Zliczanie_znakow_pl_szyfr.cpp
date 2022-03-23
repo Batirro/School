bc85ab11d94dcc8b35979a90d080dce392643729
Batirro <batirro34@protonmail.com>
2022-03-23 20:18:31 +0100

    Update Zliczanie_znakow_pl_szyfr.cpp

--- Zliczanie_znakow_pl_szyfr.cpp
+++ Zliczanie_znakow_pl_szyfr.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include <windows.h>
-#include <locale.h>
 
 using namespace std;
 
@@ -8,19 +7,20 @@ void czestosc_znakow(int Licznik[]);
 char cezar_pl(int klucz, char znak);
 string cezar_szyfr(string tekst, int klucz);
 void szyfr_pl(int klucz);
+void szyfruj_pl(int klucz);
 int klucz(int Licznik[]);
 
 const string alfabet_pl_m = "aąbcćdeęfghijklłmnńoóprsśtuvwxyzżź";
 const string alfabet_pl_d = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUVWXYZŻŹ";
 
-
 int main()
 {
 	SetConsoleCP(852);
-	setlocale(LC_ALL,"Polish");
+	setlocale(LC_ALL, "");
 	int Licznik[35] = {0};
 	czestosc_znakow(Licznik);
-	szyfr_pl(35 - klucz(Licznik));
+	klucz(Licznik);
+	szyfruj_pl(35 - klucz(Licznik));
 	cout << "Plik został pomyślnie zapisany!" << endl;
 	return 0;
 }
@@ -29,43 +29,48 @@ int klucz(int Licznik[])
 {
 	int max_z = 0, temp;
 	for (int i = 0; i < 35; i++)
+	{
+		if (Licznik[i] > Licznik[max_z])
 		{
-			if (Licznik[i] > Licznik[max_z])
-				{
-					max_z = i;
-				}
+			max_z = i;
 		}
+	}
 
 	temp = Licznik[max_z];
 	return temp;
 }
 
-char cezar_pl (int klucz, char znak)
+char cezar_pl(int klucz, char znak)
 {
 	int i = alfabet_pl_m.find(znak);
 
 	if (i >= 0 && i < 35)
-		{
-			i = (i + klucz)%35;
-			return alfabet_pl_m[i];
-		}
+	{
+		i = (i + klucz) % 35;
+		return alfabet_pl_m[i];
+	}
 	i = alfabet_pl_d.find(znak);
-	    if ( i >= 0 && i < 35)
-		{
-			i = (i + klucz)%35;
-			return alfabet_pl_d[i];
-		}
+	if (i >= 0 && i < 35)
+	{
+		i = (i + klucz) % 35;
+		return alfabet_pl_d[i];
+	}
 	return znak;
 }
 
 string cezar_szyfr(string tekst, int klucz)
 {
-
+	string szyfrogram = " ";
+	for (int i = 0; i < tekst.size(); i++)
+	{
+		szyfrogram = szyfrogram + cezar_pl(tekst[i], klucz);
+	}
+	return szyfrogram;
 }
 
 void szyfruj_pl(int klucz)
 {
-	fstream inputfile ("C:\\Users\\bartek\\Desktop\\tajne.txt");
+	fstream inputfile("C:\\Users\\bartek\\Desktop\\tajne.txt");
 	ofstream outputfile("C:\\Users\\bartek\\Desktop\\wynik.txt");
 	string tekst = " ";
 
@@ -82,33 +87,33 @@ void czestosc_znakow(int Licznik[])
 {
 	int i, j;
 	string tekst = " ";
-	for(i = 0; i < 35; i++)
-		{
-			Licznik[i] = 0;
-		}
-	fstream inputfile ("C:\\Users\\bartek\\Desktop\\tajne.txt");
+	for (i = 0; i < 35; i++)
+	{
+		Licznik[i] = 0;
+	}
+	fstream inputfile("C:\\Users\\bartek\\Desktop\\tajne.txt");
 	ofstream outputfile("C:\\Users\\bartek\\Desktop\\wynik.txt");
-	while(!inputfile.eof())
+	while (!inputfile.eof())
+	{
+		getline(inputfile, tekst);
+		for (i = 0; i < tekst.size(); i++)
 		{
-			getline(inputfile, tekst);
-			for(i = 0; i < tekst.size(); i++)
-				{
-					j = alfabet_pl_m.find(tekst[i]);
-					if (j >= 0 && j < 35)
-						{
-							Licznik[j]++;
-						}
-					j = alfabet_pl_d.find(tekst[i]);
-					if (j >= 0 && j < 35)
-						{
-							Licznik[j]++;
-						}
-				}
+			j = alfabet_pl_m.find(tekst[i]);
+			if (j >= 0 && j < 35)
+			{
+				Licznik[j]++;
+			}
+			j = alfabet_pl_d.find(tekst[i]);
+			if (j >= 0 && j < 35)
+			{
+				Licznik[j]++;
+			}
 		}
+	}
 	inputfile.close();
 	for (i = 0; i < 35; i++)
-		{
-			outputfile << alfabet_pl_d[i] << " - " << Licznik[i] << endl;
-		}
+	{
+		outputfile << alfabet_pl_d[i] << " - " << Licznik[i] << endl;
+	}
 	outputfile.close();
 }
