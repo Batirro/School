7432913751d2dab875ba7e0d4da80e5f254fce71
Batirro <batirro34@protonmail.com>
2022-04-03 12:27:54 +0200

    Update Test_c.cpp

--- Test_c.cpp
+++ Test_c.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void wczytywanie_z_pliku(int T[]);
 void wyswietl(int liczba);
-void rozklad(int T[], int liczba);
 int ilosc_pow(int T[], int liczba);
+bool szukanie_bin(int T[], int liczba);
 
 int main()
 {
@@ -13,41 +13,23 @@ int main()
 	cout << "Podaj liczbe: ";
 	cin >> liczba;
 	wyswietl(liczba);
+	cout << "Plik został pomyślnie zapisany!" << endl;
 	return 0;
 }
 
 void wczytywanie_z_pliku(int T[])
 {
-	 fstream inputfile("C:\\Users\\bartek\\Desktop\\szukanie_bin.txt");
+	 ifstream inputfile("C:\\Users\\bartek\\Desktop\\szukanie_bin.txt");
 	 for (int i = 0; i < 1023; i++)
 	 {
-		 inputfile << T[i];
+		 inputfile >> T[i];
 	 }
 	 inputfile.close();
 }
 
-void rozklad(int T[], int liczba)
-{
-	int lewo = 0, prawo = 1023, srodek;
-	while (lewo < prawo)
-	{
-		cout << lewo << " " << prawo << " ";
-		srodek = (lewo + prawo) / 2;
-		cout << srodek << endl;
-		if (T[srodek] < liczba)
-		{
-			lewo = srodek + 1;
-		}
-		else
-		{
-			prawo = srodek;
-		}
-	}
-}
-
 int ilosc_pow(int T[], int liczba)
 {
-	int lewo = 0, prawo = 1023, srodek, temp;
+	int lewo = 0, prawo = 1023, srodek, temp = 0;
 	while (lewo < prawo)
 	{
 		srodek = (lewo + prawo) / 2;
@@ -64,12 +46,54 @@ int ilosc_pow(int T[], int liczba)
 	return temp;
 }
 
+bool szukanie_bin(int T[], int liczba)
+{
+	int lewo = 0, prawo = 1023, srodek;
+	while (lewo < prawo)
+	{
+		srodek = (lewo + prawo) / 2;
+		if (T[srodek] < liczba)
+		{
+			lewo = srodek + 1;
+		}
+		else
+		{
+			prawo = srodek;
+		}
+	}
+	return (T[lewo] == liczba);
+}
+
 void wyswietl(int liczba)
 {
-	ofstream outputfile("C:\\Users\\bartek\\Desktop\\ilosc.txt");
 	int T[1024] = {0};
+	int lewo = 0, prawo = 1023, srodek;
+	ofstream outputfile("C:\\Users\\bartek\\Desktop\\odp_infa_sort.txt");
 	wczytywanie_z_pliku(T);
-	rozklad(T, liczba);
+	if (szukanie_bin(T, liczba))
+	{
+		outputfile << "Liczba " << liczba << " występuje." << endl;
+	}
+	else
+	{
+		outputfile << "Liczba " << liczba << " nie występuje." << endl;
+	}
 	outputfile << "Ilość powtórzeń to " << ilosc_pow(T, liczba) << endl;
+	outputfile << "Szczegółowy rozkład: " << endl;
+	while (lewo < prawo)
+	{
+		outputfile << lewo << " " << prawo << " ";
+		srodek = (lewo + prawo) / 2;
+		outputfile << srodek << endl;
+		if (T[srodek] < liczba)
+		{
+			lewo = srodek + 1;
+		}
+		else
+		{
+			prawo = srodek;
+		}
+	}
+	outputfile << "Numer w tablicy gdzie znajduje się liczba to " << lewo << endl;	
 	outputfile.close();
 }
