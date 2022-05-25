37b906a444a39f7eb8c8fd203383bbdb8835ab5f
Batirro <93288860+Batirro@users.noreply.github.com>
2022-05-25 09:55:43 +0200

    Update wsta-sort.cpp

--- wsta-sort.cpp
+++ wsta-sort.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-void wsta_sort(int n, int T[]);
+void wsta_sort(int n, int T[], int por, int zam);
 void tworzenie_tab(int T[], int n);
 void dodawanie_tab(int T[], int n);
+void usuwanie_plik();
 void wypisz(int T[], int n);
+void por_zam(int por, int zam);
 
 int main()
 {
-	int n = 0;
+	int n = 0, por, zam;
 	cout << "Podaj ilosc elementow tablicy: ";
 	cin >> n;
 	int T[n] = {0};
@@ -17,9 +19,10 @@ int main()
 	dodawanie_tab(T, n);
 	cout << "Nieposortowane" << endl;
 	wypisz(T, n);
-	wsta_sort(n, T);
+	wsta_sort(n, T, por, zam);
 	cout << "Posortowane" << endl;
 	wypisz(T, n);
+	usuwanie_plik();
 }
 
 void tworzenie_tab(int T[], int n)
@@ -42,20 +45,29 @@ void tworzenie_tab(int T[], int n)
     }
     tablica.close();
 }
+void usuwanie_plik()
+{
+	char deleted[] = "C:\\Users\\uczen_09\\Desktop\\tablica.txt";
+	int result = remove(deleted);
+}
 void wsta_sort(int n, int T[])
 {
 	int i, j, temp = 0;
 	for (i = 0; i < n; i++)
 	{
 		temp = T[i];
-		j = i -1;
+		j = i - 1;
 		while(j>=0 && T[j]>temp)
 		{
 			T[j+1] = T[j];
+			por++;
 			j--;
 		}
-		T[j+1] = temp; 
+		T[j+1] = temp;
+		zam++;	
 	}
+	cout << "Liczba zamian to " << zam << endl;
+	cout << "Liczba porownan to  " << por << endl;
 }
 void wypisz(int T[], int n)
 {
