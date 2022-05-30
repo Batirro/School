426c53674c60199afe944120ec015a7919fbc839
Batirro <batirro34@protonmail.com>
2022-05-30 18:05:34 +0200

    Update Szukanie_parzystych.cpp

--- Szukanie_parzystych.cpp
+++ Szukanie_parzystych.cpp
@@ -3,59 +3,77 @@
 using namespace std;
 
 int szukanie_bin(int T[], int n);
-void tworzenie_tab(int T[], int n);
+void tworzenie_tab(int T[], int n, int n1, int n2);
 void dodawanie_tab(int T[], int n);
 
 int main()
 {
-	int n, liczba;
-    cout << "Podaj ilosc elementow tablicy: ";
-    cin >> n;
-    int T[n] = {0};
-    tworzenie_tab(T, n);
-    dodawanie_tab(T, n);
+	int n, n1, n2;
+	cout << "Podaj ilosc elementow tablicy: ";
+	cin >> n;
+	cout << "Podaj ilosc elementow nieparzystych tablicy: ";
+	cin >> n1;
+	cout << "Podaj ilosc elementow parzystych tablicy: ";
+	cin >> n2;
+	int T[n] = {0};
+	tworzenie_tab(T, n, n1, n2);
+	dodawanie_tab(T, n);
+	cout << "Pierwsza liczba parzysta w tablicy to " << szukanie_bin(T, n) << endl;
+	return 0;
 }
 
-void tworzenie_tab(int T[], int n)
+void tworzenie_tab(int T[], int n, int n1, int n2)
 {
-    srand(time(NULL));
-    ofstream tablica("C:\\Users\\uczen_09\\Desktop\\tablica.txt");
+	srand(time(NULL));
+	ofstream tablica("C:\\Users\\uczen_09\\Desktop\\tablica.txt");
 	int temp1 = 0, temp2 = 0, temp = 0;
-	while(temp < n)
+	while (temp < n)
 	{
-		temp1 = rand() % 120;
-		if(temp1 % 2 != 0)
+		while (temp < n1)
 		{
-			
+			temp1 = rand() % 120;
+			if (temp1 % 2 != 0)
+			{
+				tablica << temp1;
+			}
 		}
+		while (temp < n2)
+		{
+			temp2 = rand() % 120;
+			if (temp2 % 2 == 0)
+			{
+				tablica << temp2;
+			}
+		}
+		temp++;
 	}
-    tablica.close();
+	tablica.close();
 }
 
 void dodawanie_tab(int T[], int n)
 {
-    ifstream tablica("C:\\Users\\uczen_09\\Desktop\\tablica.txt");
-    for (int i = 0; i < n; i++)
-        {
-            tablica >> T[i];
-        }
-    tablica.close();
+	ifstream tablica("C:\\Users\\uczen_09\\Desktop\\tablica.txt");
+	for (int i = 0; i < n; i++)
+	{
+		tablica >> T[i];
+	}
+	tablica.close();
 }
 
 int szukanie_bin(int T[], int n)
 {
 	int lewo = 0, prawo = n - 1, srodek = 0;
-	while (lewy < prawy)
+	while (lewo < prawo)
 	{
-		srodek = (lewy + prawy) / 2;
-		if (T[srodek] < n)
+		srodek = (lewo + prawo) / 2;
+		if (T[srodek] % 2 != 0)
 		{
-			lewy = srodek + 1;
+			lewo = srodek + 1;
 		}
 		else
 		{
-			prawy = srodek;
+			prawo = srodek;
 		}
 	}
-	return lewy;
+	return lewo;
 }
