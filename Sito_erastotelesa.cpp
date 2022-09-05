25e0ee3a065ab1154fcb8e350c412b943e4e0f98
Batirro <batirro34@protonmail.com>
2022-09-05 21:41:23 +0200

    Create Sito_erastotelesa.cpp

---
+++ Sito_erastotelesa.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sito(int T[], int n);
+void tworzenie_tab(int T[], int n);
+void wypisz(int T[], int n);
+
+int main()
+{
+    int n = 100;
+    int T[100] = {0};
+    tworzenie_tab(T, n);
+    sito(T, n);
+    wypisz(T, n);
+}
+
+void tworzenie_tab(int T[], int n)
+{
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        T[i] = rand() % 100;
+    }
+}
+
+void sito(int T[], int n)
+{
+    int j;
+    int i;
+    for (i = 2; i <= n; i++)
+    {
+        T[i] = i;
+    }
+    i = 2;
+    while ((i * i) <= n)
+    {
+        if (T[i] != 0)
+        {
+            for (j = 2; j < n; j++)
+            {
+                if (T[i] * j > n)
+                {
+                    break;
+                }
+                else
+                    T[T[i] * j] = 0;
+            }
+        }
+        i++;
+    }
+}
+
+void wypisz(int T[], int n)
+{
+    cout << "Liczby pierwsze to: " << endl;
+    for (int i = 2; i < n; i++)
+    {
+        if (T[i] != 0)
+            cout << T[i] << " ";
+    }
+}
