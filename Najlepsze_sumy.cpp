2d33000c2f11a9d0c20fe088c5030e804837c6e5
Batirro <batirro34@protonmail.com>
2022-10-23 14:28:40 +0200

    kj

--- Najlepsze_sumy.cpp
+++ Najlepsze_sumy.cpp
@@ -1,34 +1,35 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <cstdlib>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int j, k, n, i, aktsuma, maxsuma, w, liczba;
-    
+
     cout << "Podaj ilość elementów w tablicy:";
     cin >> n;
     int t[n];
-    
+
     maxsuma = 0;
     w = 0;
     cout << "Podaj liczby:" << endl;
-    while (w <= n) {
+    while (w <= n)
+    {
         cin >> liczba;
         t[w] = liczba;
         w = w + 1;
     }
-    for (j = i; j <= n - 1; j++) {
+    for (j = i; j <= n - 1; j++)
+    {
         aktsuma = 0;
-        for (k = i; k <= j; k++) {
+        for (k = i; k <= j; k++)
+        {
             aktsuma = aktsuma + t[k];
             cout << "Aktualna suma to ";
             cout << aktsuma << endl;
         }
-        if (aktsuma > maxsuma) {
+        if (aktsuma > maxsuma)
+        {
             maxsuma = aktsuma;
         }
     }
