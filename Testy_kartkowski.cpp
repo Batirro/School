7a8a06532c15e689ec940c67314c4ce346f392d7
Batirro <batirro34@protonmail.com>
2023-01-02 10:10:51 +0100

    piksele

--- Testy_kartkowski.cpp
+++ Testy_kartkowski.cpp
@@ -1,33 +1,25 @@
-#include <bits/stdc++.h>
+//Zapisz plik podajac swoje imie i nazwisko np. z_piksele_Marian_Kowalski.cpp
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
+
+void reprezentacja(vector < vector <int> > &obraz);
+void wyswietl(vector < vector<int> > obraz);
+int min_ciemny(vector < vector<int> > obraz);
+int max_jasny(vector < vector<int> > obraz); //mozna napisac razem zanjdujacy max i min w jednym przebiegu
+int os_symetrii(vector <vector<int> > obraz);
+int piksele_kontrastujace(vector <vector<int> > obraz);
+int podciag_max(vector <vector<int> > obraz); //najdluzsza linia pionowa
+
 int main()
 {
-    int liczba, akt_suma, licznik = 0;
-    vector<int> Tab;
-    cout << "Podaj ciag 20 liczb:";
-    for (int i = 0; i < 20; i++)
-    {
-        cin >> liczba;
-        Tab.push_back(liczba);
-    }
-    cout << "Podaj szukana sume: ";
-    cin >> liczba;
-
-    for (int i = 0; i < Tab.size(); i++)
-    {
-        for (int j = i; j < Tab.size(); j++)
-        {
-            akt_suma = 0;
-            for (int w = i; w <= j; w++)
-            {
-                akt_suma += Tab[w];
-                if (akt_suma == liczba)
-                {
-                    licznik++;
-                }
-            }
-        }
-    }
-    cout << "Ilosc podciagow o podanej sumie to " << licznik << endl;
+	
+	return 0;
 }
+//--------------------------------------------------------
+//ponizej zdefiniuj kazda z funkcji, zmien nazwe pliku np. kowalski_jan_piksele.cpp
